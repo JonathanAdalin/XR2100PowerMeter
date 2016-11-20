135e21b7869164f71cc6d93e7baa900b5a3d3022
Jonathan Adalin <JonathanAdalin@gmail.com>
2016-11-20 15:30:21 -0500

    Added CRC8 calculation function from documentation

--- XR2100PowerMeter/main.cpp
+++ XR2100PowerMeter/main.cpp
@@ -9,7 +9,27 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     std::cout << "Hello, World!\n";
-    return 0;
+}
+
+// Function taken from XR2100 documentation
+unsigned char CalcCRC8(unsigned char *data){
+    
+    unsigned char loopCounter;
+    unsigned char crc8;
+    unsigned char a;
+    
+    crc8 = 0;
+    while (*data){
+        a = *data++;
+        for (loopCounter = 0; loopCounter < 8; loopCounter++, a >>= 1){
+            if ((a ^ crc8) & 0x01){
+                crc8 ^= 0x18;
+                crc8 >>= 1;
+                crc8 |= 0x80;
+            }   else
+                crc8 >>= 1;
+        }
+    }
+    return crc8;
 }
