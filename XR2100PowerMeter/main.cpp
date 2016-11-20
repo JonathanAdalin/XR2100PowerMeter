003d4729b8fe5d5b0c4d335715cbc50bf9068a89
Jonathan Adalin <JonathanAdalin@gmail.com>
2016-11-20 15:01:57 -0500

    Initial Commit

---
+++ XR2100PowerMeter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XR2100PowerMeter
+//
+//  Created by Jonathan Adalin on 2016-11-20.
+//  Copyright © 2016 Jonathan Adalin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
